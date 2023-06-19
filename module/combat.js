0cb0213d7989eec3466d16830421aeb74dd0e954
dmdorman <drewdorguy@gmail.com>
2023-06-18 21:28:29 -0500

    Update combat.js

--- module/combat.js
+++ module/combat.js
@@ -402,10 +402,10 @@ export class HeroSystem6eCombat extends Combat {
 
                 switch(item.system.id) {
                     case ('LIGHTNING_REFLEXES_ALL'): {
-                        const levels = item.system.LEVELS || item.system.other.levels
+                        const levels = item.system.LEVELS || item.system.other.levels || 0
                         const lightning_reflex_initiative = (parseInt(dexValue) + parseInt(levels)) + (parseInt(initativeValue) / 100)
                         
-                        const alias = item.system.OPTION_ALIAS || item.system.other.option_alias
+                        const alias = item.system.OPTION_ALIAS || item.system.other.option_alias || 'None'
                         const lightning_reflex_alias = '(' + alias + ')'
                         
                         allInitiatives.push([name, lightning_reflex_alias, lightning_reflex_initiative])
