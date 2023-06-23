2f9984469f95a97547e349b7e0351cc064931849
aeauseth <aeauseth@users.noreply.github.com>
2023-06-22 21:12:50 -0400

    added additional "nonfunctional" status icons

--- module/actor/actor-active-effects.js
+++ module/actor/actor-active-effects.js
@@ -1,12 +1,27 @@
 export class HeroSystem6eActorActiveEffects extends ActiveEffect {
+
+    // Rhair3 suggestion:
+    // First line is Dead / Unconscious/Asleep/Stunned
+    // Second Line: Prone/ Entangled/Paralysed/Flying
+    // Third Line: Blind/ Deaf/ Mute/Frightened
+    // Forth Line: Burning/Frozen/Shocked/Corroding
+    // Fith Line: Bleeding/Diseased/Poisoned/Cursed
+    // Sixth Line: Regenerating/Degenerationg/Aid/Drain
+    // seventh Line: Invisible/ Targeted/ Marked/ Blessed
+    // Last Line: Fire Shield/ Ice Shield/ Magic Shield/ Holy Shield
+
     static getEffects() {
-        return [
-            HeroSystem6eActorActiveEffects.stunEffect,
-            HeroSystem6eActorActiveEffects.bleedingEffect,
-            HeroSystem6eActorActiveEffects.unconsciousEffect,
-            HeroSystem6eActorActiveEffects.deadEffect,
-            HeroSystem6eActorActiveEffects.blindEffect,
-        ];
+        return Object.keys(HeroSystem6eActorActiveEffects)
+            .filter(o => o.includes('Effect'))
+            .sort()
+            .map(o => HeroSystem6eActorActiveEffects[o])
+        // return [
+        //     HeroSystem6eActorActiveEffects.blindEffect,
+        //     HeroSystem6eActorActiveEffects.bleedingEffect,
+        //     HeroSystem6eActorActiveEffects.deadEffect,
+        //     HeroSystem6eActorActiveEffects.stunEffect,
+        //     HeroSystem6eActorActiveEffects.unconsciousEffect,
+        // ];
     }
 
     // A Stunned character’s DCV and DMCV are instantly halved.
@@ -60,10 +75,111 @@ export class HeroSystem6eActorActiveEffects extends ActiveEffect {
         label: "EFFECT.StatusBlind",
         id: "blind",
         icon: 'icons/svg/blind.svg',
-        changes: [
-            { key: "system.characteristics.ocv.value", value: 0.5, mode: CONST.ACTIVE_EFFECT_MODES.MULTIPLY },
-            { key: "system.characteristics.dcv.value", value: 0.5, mode: CONST.ACTIVE_EFFECT_MODES.MULTIPLY }
-        ]
+        // changes: [
+        //     { key: "system.characteristics.ocv.value", value: 0.5, mode: CONST.ACTIVE_EFFECT_MODES.MULTIPLY },
+        //     { key: "system.characteristics.dcv.value", value: 0.5, mode: CONST.ACTIVE_EFFECT_MODES.MULTIPLY }
+        // ]
+    };
+
+    static asleepEffect = {
+        label: "EFFECT.StatusAsleep",
+        id: "asleep",
+        icon: 'icons/svg/sleep.svg',
+        // changes: [
+        //     { key: "system.characteristics.ocv.value", value: 0.5, mode: CONST.ACTIVE_EFFECT_MODES.MULTIPLY },
+        //     { key: "system.characteristics.dcv.value", value: 0.5, mode: CONST.ACTIVE_EFFECT_MODES.MULTIPLY }
+        // ]
+    };
+
+    static proneEffect = {
+        id: "prone",
+        name: "EFFECT.StatusProne",
+        icon: "icons/svg/falling.svg"
+    };
+
+    static entangledEffect = {
+        id: "entangled",
+        name: "Entangled",
+        icon: "icons/svg/net.svg"
+    };
+
+    static paralysisEffect = {
+        id: "paralysis",
+        name: "EFFECT.StatusParalysis",
+        icon: "icons/svg/paralysis.svg"
+    };
+
+    static flyingEffect = {
+        id: "fly",
+        name: "EFFECT.StatusFlying",
+        icon: "icons/svg/wing.svg"
+    };
+
+    static deafEffect = {
+        id: "deaf",
+        name: "EFFECT.StatusDeaf",
+        icon: "icons/svg/deaf.svg"
+    };
+    static silencedEffect = {
+        id: "silence",
+        name: "EFFECT.StatusSilenced",
+        icon: "icons/svg/silenced.svg"
+    };
+    static freightenedEffect = {
+        id: "fear",
+        name: "EFFECT.StatusFear",
+        icon: "icons/svg/terror.svg"
+    };
+
+    static regenEffect = {
+        id: "regen",
+        name: "EFFECT.StatusRegen",
+        icon: "icons/svg/regen.svg"
+    };
+
+    static aidEffect = {
+        id: "upgrade",
+        name: "Aid",
+        icon: "icons/svg/upgrade.svg"
+    };
+
+    static drainEffect = {
+        id: "downgrade",
+        name: "Drain",
+        icon: "icons/svg/downgrade.svg"
+    };
+
+    static invisibleEffect = {
+        id: "invisible",
+        name: "EFFECT.StatusInvisible",
+        icon: "icons/svg/invisible.svg"
+    };
+
+    static targetedEffect = {
+        id: "target",
+        name: "EFFECT.StatusTarget",
+        icon: "icons/svg/target.svg"
+    };
+
+    static shieldFireEffect = {
+        id: "fireShield",
+        name: "Shield: Fire",
+        icon: "icons/svg/fire-shield.svg"
+    };
+    static shieldIceEffect = {
+        id: "coldShield",
+        name: "Shield: Ice",
+        icon: "icons/svg/ice-shield.svg"
+    };
+    static shieldMagicEffect = {
+        id: "magicShield",
+        name: "Shield: Magic",
+        icon: "icons/svg/mage-shield.svg"
+    };
+    static shieldHolyEffect = {
+        id: "holyShield",
+        name: "Shield: holy",
+        icon: "icons/svg/holy-shield.svg"
     };
 
     /** @override */
