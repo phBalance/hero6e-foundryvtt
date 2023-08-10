2cf0cf4ea81f16652381f8f5b9659f021c37679e
aeauseth <aeauseth@users.noreply.github.com>
2023-08-09 19:30:16 -0700

    Improved ItemAttackFormApplication

--- module/item/item-attack-application.js
+++ module/item/item-attack-application.js
@@ -17,6 +17,9 @@ export class ItemAttackFormApplication extends FormApplication {
       template: `systems/hero6efoundryvttv2/templates/attack/item-attack-application.hbs`,
       id: 'item-attack-form-application',
       //title: `${actor.name} roll to hit`,
+      //resizable: true,
+      closeOnSubmit: false, // do not close when submitted
+      submitOnChange: true, // submit when any input changes
     });
     return options
   }
@@ -29,12 +32,20 @@ export class ItemAttackFormApplication extends FormApplication {
 
   activateListeners(html) {
     super.activateListeners(html);
-    html.find(".combat-skill-levels input").change((event) => this._updateCsl(event, html));
+    //html.find(".combat-skill-levels input").change((event) => this._updateCsl(event, html));
   }
 
   async _updateObject(event, formData) {
     console.log(formData);
-    return _processAttackOptions(this.data.item, formData);
+
+
+    if (event.submitter) {
+      this.close();
+      return _processAttackOptions(this.data.item, formData);
+    }
+
+    this._updateCsl(event, formData)
+
 
   }
 
@@ -42,25 +53,34 @@ export class ItemAttackFormApplication extends FormApplication {
     const item = this.data.item
     // Combat Skill Levels (update SKILL if changed)
     const csl = CombatSkillLevelsForAttack(item);
-    const checked = formData.find(".combat-skill-levels input:checked");
-    if (csl && checked) {
-      let updateRequired = false;
-      for (let input of checked) {
-        let m = input.name.match(/\.(\w+)\.(\d+)/);
-        let name = m[1];
-        let idx = m[2];
-
-        if (csl.skill.system.csl[idx] != input.value) {
-          csl.skill.system.csl[idx] = input.value;
-          updateRequired = true;
-        }
-      }
-      if (updateRequired) {
+    for (const key of Object.keys(formData).filter(o=> o.match(/\.(\w+)\.(\d+)/) )) {
+      const value = formData[key];
+      const idx = parseInt(key.match(/\d+$/));
+      if (csl.skill.system.csl[idx] != value) {
+        csl.skill.system.csl[idx] = value;
         await csl.skill.update({ 'system.csl': csl.skill.system.csl });
       }
 
-
     }
+    //const checked = formData.find(".combat-skill-levels input:checked");
+    //if (csl && checked) {
+    //   let updateRequired = false;
+    //   for (let input of checked) {
+    //     let m = input.name.match(/\.(\w+)\.(\d+)/);
+    //     let name = m[1];
+    //     let idx = m[2];
+
+    //     if (csl.skill.system.csl[idx] != input.value) {
+    //       csl.skill.system.csl[idx] = input.value;
+    //       updateRequired = true;
+    //     }
+    //   }
+    //   if (updateRequired) {
+    //     await csl.skill.update({ 'system.csl': csl.skill.system.csl });
+    //   }
+
+
+    // }
   }
 }
 
