270c99db9b8546c18e95ba383c8ce78d352a5964
phBalance <phBalance 29824554+phBalance@users.noreply.github.com>
2023-12-03 19:51:07 -0700

    feat(item): show warnings for invalid source/target for TRANSFER

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -978,17 +978,18 @@ export class HeroSystem6eActor extends Actor {
 
         // Warn about invalid adjustment targets
         for (const item of this.items.filter(item => getPowerInfo({ item: item }).powerType?.includes("adjustment"))) {
-            const adjustmentTargets = item.system.INPUT
-
-            if (adjustmentTargets) {
-                for (const rawInput of adjustmentTargets.split(",")) {
-                    const upperCasedInput = rawInput.toUpperCase().trim()
-                    if (!Object.keys(AdjustmentSources(this)).includes(upperCasedInput)) {
-                        await ui.notifications.warn(`${this.name} has an unsupported target (${rawInput}) for "${item.name}". Use characteristic abbreviations or power names separated by commas.`, {console: true, permanent: true});
+            const splitTargetsAndSources = item.system.INPUT ? item.system.INPUT.split(" -> ") : []
+            if ((item.XMLID !== "TRANSFER" && splitTargetsAndSources.length === 1 && splitTargetsAndSources[0]) || (item.XMLID === "TRANSFER" && splitTargetsAndSources.length === 2 && splitTargetsAndSources[0] && splitTargetsAndSources[1])) {
+                for (let i = 0; i < splitTargetsAndSources.length; ++i) {
+                    for (const rawAdjustmentSourcesOrTargets of splitTargetsAndSources[i].split(",")) {
+                        const upperCasedInput = rawAdjustmentSourcesOrTargets.toUpperCase().trim()
+                        if (!Object.keys(AdjustmentSources(this)).includes(upperCasedInput)) {
+                            await ui.notifications.warn(`${this.name} has an unsupported source or target (${rawAdjustmentSourcesOrTargets}) for "${item.name}". Use characteristic abbreviations or power names separated by commas.`, {console: true, permanent: true});
+                        }
                     }
                 }
             } else {
-                await ui.notifications.warn(`${this.name} has no target for "${item.name}". Use characteristic abbreviations or power names separated by commas.`, {console: true, permanent: true});
+                await ui.notifications.warn(`${this.name} has no source or target for "${item.name}". Use characteristic abbreviations or power names separated by commas. ${item.XMLID === "TRANSFER" ? "Sources and targets should be separated by \" -> \"." : "" }`, {console: true, permanent: true});
             }
         }
 
