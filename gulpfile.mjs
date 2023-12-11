f991d998cd551047c4030fa9d71c4479a144a654
phBalance <phBalance 29824554+phBalance@users.noreply.github.com>
2023-12-10 20:21:27 -0700

    refactor(gulp): adapt gulp to new prettier format

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,53 +1,55 @@
-import gulp from "gulp"
-import gulpAutoprefix from "gulp-autoprefixer"
-import gulpEslintNew from "gulp-eslint-new"
-import gulpPrettier from "gulp-prettier"
-import gulpSass from "gulp-sass"
-import * as dartSass from "sass"
+import gulp from "gulp";
+import gulpAutoPrefix from "gulp-autoprefixer";
+import gulpEslintNew from "gulp-eslint-new";
+import gulpPrettier from "gulp-prettier";
+import gulpSass from "gulp-sass";
+import * as dartSass from "sass";
 
-const sass = gulpSass(dartSass)
+const sass = gulpSass(dartSass);
 
-const SASS_FILES = ["scss/**/*.scss"]
-const JAVASCRIPT_FILES = ["**/*.js","!node_modules/**"]
+const SASS_FILES = ["scss/**/*.scss"];
+const JAVASCRIPT_FILES = ["**/*.js", "!node_modules/**"];
 
 /* ----------------------------------------- */
 /*  Source Code Standard Validation
 /* ----------------------------------------- */
 
-const eslintDefaultConfig = { overrideConfigFile: "./.eslintrc.json" }
-const eslintFixConfig = { fix: true }
+const eslintDefaultConfig = { overrideConfigFile: "./.eslintrc.json" };
+const eslintFixConfig = { fix: true };
 
 function validateFilesByLint() {
-  return gulp.src(JAVASCRIPT_FILES)
-    .pipe(gulpEslintNew(eslintDefaultConfig))
-    .pipe(gulpEslintNew.formatEach("compact", process.stderr))
+    return gulp
+        .src(JAVASCRIPT_FILES)
+        .pipe(gulpEslintNew(eslintDefaultConfig))
+        .pipe(gulpEslintNew.formatEach("compact", process.stderr));
 }
-const lint = gulp.series(validateFilesByLint)
+const lint = gulp.series(validateFilesByLint);
 
 function autoFixFilesByLint() {
-  return gulp.src(JAVASCRIPT_FILES)
-    .pipe(gulpEslintNew({...eslintDefaultConfig, ...eslintFixConfig }))
-    .pipe(gulpEslintNew.fix())
-    .pipe(gulpEslintNew.formatEach("compact", process.stderr))
-    .pipe(gulpEslintNew.failAfterError())
+    return gulp
+        .src(JAVASCRIPT_FILES)
+        .pipe(gulpEslintNew({ ...eslintDefaultConfig, ...eslintFixConfig }))
+        .pipe(gulpEslintNew.fix())
+        .pipe(gulpEslintNew.formatEach("compact", process.stderr))
+        .pipe(gulpEslintNew.failAfterError());
 }
-const lintAutoFix = gulp.series(autoFixFilesByLint)
+const lintAutoFix = gulp.series(autoFixFilesByLint);
 
 function validateFilesByPrettier() {
-  return gulp.src(JAVASCRIPT_FILES)
-    .pipe(gulpPrettier.check())
+    return gulp.src(JAVASCRIPT_FILES).pipe(gulpPrettier.check());
 }
-const prettier = gulp.series(validateFilesByPrettier)
+const prettier = gulp.series(validateFilesByPrettier);
 
 function autoFixFilesByPrettier() {
-  return gulp.src(JAVASCRIPT_FILES)
-    .pipe(gulpPrettier())
-    .pipe(gulp.dest(file => file.base))
+    return gulp
+        .src(JAVASCRIPT_FILES)
+        .pipe(gulpPrettier())
+        .pipe(gulp.dest((file) => file.base));
 }
-const prettierAutoFix = gulp.series(autoFixFilesByPrettier)
+const prettierAutoFix = gulp.series(autoFixFilesByPrettier);
 
-const validate = gulp.parallel(validateFilesByLint, validateFilesByPrettier)
-const autoFix = gulp.series(autoFixFilesByPrettier, autoFixFilesByLint)
+const validate = gulp.parallel(validateFilesByLint, validateFilesByPrettier);
+const autoFix = gulp.series(autoFixFilesByPrettier, autoFixFilesByLint);
 
 /* ----------------------------------------- */
 /*  Compile Sass
@@ -55,59 +57,52 @@ const autoFix = gulp.series(autoFixFilesByPrettier, autoFixFilesByLint)
 
 // Small error handler helper function.
 function handleError(err) {
-  console.error(err.toString())
-  this.emit("end")
+    console.error(err.toString());
+    this.emit("end");
 }
 
 function compileSass() {
-  // Configure options for sass output. For example, "expanded" or "nested"
-  let options = {
-    outputStyle: "expanded"
-  }
-  return gulp.src(SASS_FILES)
-    .pipe(
-      sass
-        .sync(options)
-        .on("error", handleError)
-    )
-    .pipe(gulpAutoprefix({
-      cascade: false
-    }))
-    .pipe(gulp.dest("./css"))
+    // Configure options for sass output. For example, "expanded" or "nested"
+    let options = {
+        outputStyle: "expanded",
+    };
+    return gulp
+        .src(SASS_FILES)
+        .pipe(sass.sync(options).on("error", handleError))
+        .pipe(
+            gulpAutoPrefix({
+                cascade: false,
+            }),
+        )
+        .pipe(gulp.dest("./css"));
 }
-const css = gulp.series(compileSass)
+const css = gulp.series(compileSass);
 
 /* ----------------------------------------- */
 /*  Watch Updates
 /* ----------------------------------------- */
 
 function watchUpdates() {
-  gulp.watch(SASS_FILES, css)
+    gulp.watch(SASS_FILES, css);
 }
 
 /* ----------------------------------------- */
 /*  Default Task
 /* ----------------------------------------- */
 
-const defaultGulpTask = gulp.series(
-  gulp.parallel(compileSass),
-  watchUpdates
-)
+const defaultGulpTask = gulp.series(gulp.parallel(compileSass), watchUpdates);
 
 /* ----------------------------------------- */
 /*  Export Tasks
 /* ----------------------------------------- */
 
 export {
-  defaultGulpTask as default,
-
-  css,
-
-  lint,
-  prettier,
-  validate,
-
-  autoFix,
-  lintAutoFix,
-  prettierAutoFix,
-}
+    defaultGulpTask as default,
+    css,
+    lint,
+    prettier,
+    validate,
+    autoFix,
+    lintAutoFix,
+    prettierAutoFix,
+};
