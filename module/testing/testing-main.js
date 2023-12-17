543371c61148af5a06ae80f26daaa7bbb5eca797
phBalance <phBalance 29824554+phBalance@users.noreply.github.com>
2023-12-17 16:39:51 -0700

    test(quench): provide a global method to run the test suite in a loop

--- module/testing/testing-main.js
+++ module/testing/testing-main.js
@@ -27,3 +27,56 @@ Hooks.on("quenchReady", (quench) => {
     registerUploadTests(quench);
     registerFullTests(quench);
 });
+
+// Helper function to run all tests from browser console.
+//
+// For browsers that support top level await (e.g. Chrome) you can just:
+// console.log(`Test suite result: ${await window.herosystem6eRunTests(100)}`);
+//
+// For other browsers you can just:
+// window.herosystem6eRunTests(100).catch((err) => console.error(`Error with test runs: ${err.message}`)).then((result) => console.log(`Finished test runs`))
+//
+window.herosystem6eRunTests = async (numLoops = 1) => {
+    if (!game.modules.get("quench")?.active) {
+        ui.notifications.warn(game.i18n.localize("Warning.Quench.Active"));
+        throw new Error("Quench not active. Cannot run tests.");
+    }
+
+    for (let i = 0; i < numLoops; ++i) {
+        try {
+            console.log(`Start test run ${i + 1} of ${numLoops}`);
+            await runTestSuiteOnce();
+        } catch (err) {
+            console.error(`Test run failed: ${err}`);
+            return err;
+        }
+    }
+
+    console.log(`Completed ${numLoops} test runs`);
+
+    return true;
+};
+
+async function runTestSuiteOnce() {
+    const mochaRunner = await quench.runBatches("hero6efoundryvttv2.**", {
+        updateSnapshots: false,
+        preSelectedOnly: false,
+        json: false,
+    });
+
+    return new Promise((resolve, reject) => {
+        const timeoutId = setTimeout(() => {
+            mochaRunner.abort();
+            mochaRunner.dispose();
+            reject("Test suite took too long - aborting runs");
+        }, 20 * 1000 /* 20 seconds */);
+
+        mochaRunner.on("end", () => {
+            // Clean up from the run.
+            mochaRunner.dispose();
+            clearTimeout(timeoutId);
+
+            resolve(true);
+        });
+    });
+}
