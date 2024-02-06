fbaa3613fcb3407602bd0c0cec36e94af4a4ae89
Martin <martinjwhalen@gmail.com>
2024-02-06 02:45:33 -0500

    update the right file this time

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -1796,35 +1796,13 @@ export class HeroSystem6eActor extends Actor {
 
         return changed;
     }
-
+   
     updateRollable(key) {
         const characteristic = this.system.characteristics[key];
         if (characteristic.type === "rollable") {
-            if (characteristic.value === 0) {
-                characteristic.roll = 8;
-            } else if (characteristic.value <= 2) {
-                characteristic.roll = 9;
-            } else if (characteristic.value <= 7) {
-                characteristic.roll = 10;
-            } else if (characteristic.value <= 12) {
-                characteristic.roll = 11;
-            } else if (characteristic.value <= 17) {
-                characteristic.roll = 12;
-            } else if (characteristic.value <= 22) {
-                characteristic.roll = 13;
-            } else if (characteristic.value <= 27) {
-                characteristic.roll = 14;
-            } else if (characteristic.value <= 32) {
-                characteristic.roll = 15;
-            } else if (characteristic.value <= 37) {
-                characteristic.roll = 16;
-            } else if (characteristic.value <= 42) {
-                characteristic.roll = 17;
-            } else if (characteristic.value <= 47) {
-                characteristic.roll = 18;
-            } else if (characteristic.value <= 52) {
-                characteristic.roll = 19;
-            } else {
+            if (characteristic.value >= 0 && characteristic.value <= 52) {
+                characteristic.roll = characteristic.roll = 9 + (characteristic.value * 0.2);
+            } else if (characteristic.value > 52) {
                 characteristic.roll = 20;
             }
         }
