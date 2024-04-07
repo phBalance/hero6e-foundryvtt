383f564fc64ff684495ae5c2b7313787211ce74f
aeauseth <aeauseth@users.noreply.github.com>
2024-04-07 14:30:16 -0700

    Removed unused HeroSystem6eToHitCard and references

--- module/card/card-helpers.mjs
+++ module/card/card-helpers.mjs
@@ -1,16 +1,13 @@
 import { HeroSystem6eCard } from "./card.mjs";
-import { HeroSystem6eToHitCard } from "./toHit-card.mjs";
 import * as Attack from "../item/item-attack.mjs";
 
 export class HeroSystem6eCardHelpers {
     static onMessageRendered(html) {
-        HeroSystem6eToHitCard.onMessageRendered(html);
         Attack.onMessageRendered(html);
     }
 
     static chatListeners(html) {
         HeroSystem6eCard.chatListeners(html);
-        HeroSystem6eToHitCard.chatListeners(html);
         Attack.chatListeners(html);
     }
 }
