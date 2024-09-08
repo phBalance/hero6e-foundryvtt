283885034bee2a785adbb78ef9c8c7bed58e144d
aeauseth <aeauseth@users.noreply.github.com>
2024-09-08 10:41:15 -0700

    Fixed issue where adding/removing combatants messed up playeres combat tracker. #1208

--- module/combatTracker.mjs
+++ module/combatTracker.mjs
@@ -47,7 +47,7 @@ export class HeroSystem6eCombatTracker extends CombatTracker {
 
         // Looks like super.getData returns a minimal combatant, need to add flags.
         // Handle segments while were at it (as it is stored in flags.segment)
-        let activeSegment = 12;
+        let activeSegment = context.combat?.combatant?.flags.segment || 12;
         for (let t = 0; t < context.turns.length; t++) {
             const turn = context.turns[t];
             turn.flags = context.combat.combatants.find((c) => c.id === turn.id)?.flags;
