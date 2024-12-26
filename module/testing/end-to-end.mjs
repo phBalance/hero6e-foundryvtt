4af80be3d553d1720ffb0918c8449705b53563a5
aeauseth <aeauseth@users.noreply.github.com>
2024-12-25 22:08:44 -0800

    better testing

--- module/testing/end-to-end.mjs
+++ module/testing/end-to-end.mjs
@@ -57,37 +57,32 @@ export class HeroSystem6eEndToEndTest {
         await this.createTestActors();
 
         // AID 6 multiple characteristics + stacking
-        // if (!(await this.testAdjustmentStacking(this.token6, this.token5, "AID", "STR, DEX"))) return;
+        if (!(await this.testAdjustmentStacking(this.token6, this.token5, "AID", "STR, DEX"))) return;
         if (!(await this.testAdjustmentStacking(this.token6, this.token5, "AID", "END, POWERDEFENSE"))) return;
 
         // AID 6
-        // if (!(await this.testAdjustmentStacking(this.token6, this.token5, "AID", "STR"))) return;
-        // if (!(await this.testAdjustmentStacking(this.token6, this.token5, "AID", "PD"))) return;
-        // if (!(await this.testAdjustmentStacking(this.token6, this.token5, "AID", "END"))) return;
-        // if (!(await this.testAdjustmentStacking(this.token6, this.token5, "AID", "DEX"))) return;
+        await this.token5.actor.FullHealth();
+        await this.token6.actor.FullHealth();
+        if (!(await this.testAdjustmentStacking(this.token6, this.token5, "AID", "STR"))) return;
+        if (!(await this.testAdjustmentStacking(this.token6, this.token5, "AID", "PD"))) return;
+        if (!(await this.testAdjustmentStacking(this.token6, this.token5, "AID", "END"))) return;
+        if (!(await this.testAdjustmentStacking(this.token6, this.token5, "AID", "DEX"))) return;
 
         // // AID 5
-        // await this.token5.actor.FullHealth();
-        // await this.token6.actor.FullHealth();
-        // if (!(await this.testAdjustment(this.token5, this.token5, "AID", "DEX"))) return;
+        await this.token5.actor.FullHealth();
+        await this.token6.actor.FullHealth();
+        if (!(await this.testAdjustmentStacking(this.token5, this.token5, "AID", "DEX"))) return;
 
-        // // DRAIN 6
-        // await this.token5.actor.FullHealth();
-        // await this.token6.actor.FullHealth();
-        // if (!(await this.testAdjustmentStacking(this.token6, this.token5, "DRAIN", "STR"))) return;
-        // if (!(await this.testAdjustmentStacking(this.token6, this.token5, "DRAIN", "DEX"))) return;
+        // DRAIN 6
+        await this.token5.actor.FullHealth();
+        await this.token6.actor.FullHealth();
+        if (!(await this.testAdjustmentStacking(this.token6, this.token5, "DRAIN", "STR"))) return;
+        if (!(await this.testAdjustmentStacking(this.token6, this.token5, "DRAIN", "DEX"))) return;
 
-        // // DRAIN 5
-        // await this.token5.actor.FullHealth();
-        // await this.token6.actor.FullHealth();
-        // if (!(await this.testAdjustment(this.token5, this.token6, "DRAIN", "DEX"))) return;
-
-        // AID 6 stacking
-        //await this.token5.actor.FullHealth();
-        //await this.token6.actor.FullHealth();
-        //if (!(await this.testAdjustmentStacking(this.token6, this.token5, "AID", "STR", 1))) return;
-        //if (!(await this.testAdjustmentStacking(this.token6, this.token5, "AID", "DEX"))) return;
-        //if (!(await this.testAdjustmentStacking(this.token6, this.token5, "AID", "END"))) return;
+        // DRAIN 5
+        await this.token5.actor.FullHealth();
+        await this.token6.actor.FullHealth();
+        if (!(await this.testAdjustmentStacking(this.token5, this.token6, "DRAIN", "DEX"))) return;
 
         CONFIG.debug.adjustmentFadeKeep = false;
 
