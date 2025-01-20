7982e5d62d55176471b256a934af1f4e899fc8c9
aeauseth <aeauseth@users.noreply.github.com>
2025-01-20 14:50:17 -0800

    Fixed negative defenses #1695

--- module/utility/defense.mjs
+++ module/utility/defense.mjs
@@ -198,7 +198,7 @@ export function getActorDefensesVsAttack(targetActor, attackItem, options = {}) 
     // Sort tags by value, shortDesc.  Get rid of 0 values.
     actorDefenses.defenseTags = actorDefenses.defenseTags
         .sort((a, b) => b.value - a.value || a.shortDesc.localeCompare(b.shortDesc))
-        .filter((o) => o.value);
+        .filter((o) => o.value > 0);
 
     // Totals
     for (const tag of actorDefenses.defenseTags) {
