d90eac29096eb9723df8ab0159dcdfcb9d060e6e
aeauseth <aeauseth@users.noreply.github.com>
2025-01-24 09:28:17 -0800

    fix AID with mental vulnerability

--- module/utility/defense.mjs
+++ module/utility/defense.mjs
@@ -340,6 +340,12 @@ export function defenseConditionalCheckedByDefault(defenseItem, attackingItem) {
 }
 
 export async function getConditionalDefenses(token, item, avad) {
+    // Some attacks have no defenses
+    if (item.baseInfo.hasNoDefense) {
+        console.debug(`${item.name}/${item.system.XMLID} has no defense`);
+        return { ignoreDefenseIds: [], conditionalDefenses: [] };
+    }
+
     let ignoreDefenseIds = [];
     let conditionalDefenses = token.actor.items.filter(
         (o) =>
