cdf1e2cb689ef9bfb6b76362571aa519a744ad54
aeauseth <aeauseth@users.noreply.github.com>
2025-02-01 22:12:01 -0800

    missing baseinfo is info not warn for now

--- module/item/adder.mjs
+++ module/item/adder.mjs
@@ -31,7 +31,7 @@ export class HeroSystem6eAdder {
 
         if (!this.#baseInfo) {
             if (!window.warnAdder?.includes(this.XMLID)) {
-                console.warn(
+                console.info(
                     `${this.item?.actor.name}/${this.item?.name}/${this.item?.system.XMLID}/${this.XMLID}: missing baseInfo.`,
                     this,
                 );
