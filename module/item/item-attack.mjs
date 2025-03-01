f72326074084fb95f0b9762d8af210ed091081e4
phBalance <29824554+phBalance@users.noreply.github.com>
2025-03-01 13:15:38 -0700

    fix(active effect): add missing awaits + formatting

--- module/item/item-attack.mjs
+++ module/item/item-attack.mjs
@@ -2694,7 +2694,7 @@ export async function _onApplyDamageToEntangle(attackItem, token, originalRoll, 
             entangleAE.update({ changes: entangleAE.changes });
             effectsFinal = `Entangle has ${newBody} BODY remaining.`;
         } else {
-            entangleAE.parent.removeActiveEffect(entangleAE);
+            await entangleAE.parent.removeActiveEffect(entangleAE);
             effectsFinal = `Entangle was destroyed.`;
         }
     }
