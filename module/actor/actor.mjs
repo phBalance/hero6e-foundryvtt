cdb648cbd2301af6ff66850b0c93dccfb0a09325
aeauseth <aeauseth@users.noreply.github.com>
2025-03-15 18:19:13 -0700

    Removed old LEVELS reference

--- module/actor/actor.mjs
+++ module/actor/actor.mjs
@@ -2534,8 +2534,7 @@ export class HeroSystem6eActor extends Actor {
         for (const key of Object.keys(this.system.characteristics)) {
             //if (key.toLowerCase() === "spd") debugger;
 
-            let newValue = parseInt(this.system?.[key.toUpperCase()]?.LEVELS || 0); // uppercase?  LEVELS?  This probably hasn't worked in a long time!
-            newValue += this.getCharacteristicBase(key) || 0; // 5e will have empty base for ocv/dcv and other figured characteristics
+            let newValue = this.getCharacteristicBase(key) || 0; // 5e will have empty base for ocv/dcv and other figured characteristics
             if (this.system.is5e && key === "spd") {
                 // SPD is always an integer, but in 5e due to figured characteristics, the base can be fractional.
                 newValue = Math.floor(newValue);
