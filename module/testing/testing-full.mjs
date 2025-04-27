9face4556a2182472e1e01de9acf7aa63bd6400d
aeauseth <aeauseth@users.noreply.github.com>
2025-04-26 18:19:01 -0700

    #2228

--- module/testing/testing-full.mjs
+++ module/testing/testing-full.mjs
@@ -5368,7 +5368,7 @@ export function registerFullTests(quench) {
                     });
                 });
 
-                describe.only("can load older HDC format - GitHub issue #2229", function () {
+                describe("can load older HDC format - GitHub issue #2229", function () {
                     const contents = `
                         <?xml version="1.0" encoding="UTF-8"?>
                         <CHARACTER version="2.0">
