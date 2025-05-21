2f1a7309e8be1ce00fb3e4fa804473ce18c03045
aeauseth <aeauseth@users.noreply.github.com>
2025-05-21 09:26:18 -0700

    V13 Compendium bypass for _onCreateEntry #2330

--- module/compendiumDirectory.mjs
+++ module/compendiumDirectory.mjs
@@ -8,86 +8,91 @@ export class HeroSystem6eCompendiumDirectory extends CompendiumDirectory {
         super(...args);
     }
 
-    async _onCreateEntry(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        const li = event.currentTarget.closest(".directory-item");
-        const targetFolderId = li ? li.dataset.folderId : null;
-        const types = CONST.COMPENDIUM_DOCUMENT_TYPES.map((documentName) => {
-            return {
-                value: documentName,
-                label: game.i18n.localize(getDocumentClass(documentName).metadata.label),
-            };
-        });
-        game.i18n.sortObjects(types, "label");
-        const folders = this.collection._formatFolderSelectOptions();
-        let html = await renderTemplate("templates/sidebar/compendium-create.html", {
-            types,
-            folders,
-            folder: targetFolderId,
-            hasFolders: folders.length >= 1,
-        });
-        html = html.replace(
-            "Document.</p>",
-            `Document.</p><label>Hero Designer Prefab</label><input name="upload" class="upload" type="file" accept=".hdp"></input>`,
-        );
-
-        function handleRender(html) {
-            html.on("change", "input.upload", (event) => {
-                console.log(event, event.target.files[0]);
-                const reader = new FileReader();
-                reader.onload = async function (event) {
-                    const contents = event.target.result;
-
-                    const parser = new DOMParser();
-                    const xmlDoc = parser.parseFromString(contents, "text/xml");
-                    // TODO create the Item compendium
-                    HeroSystem6eCompendiumDirectory.uploadFromXml(xmlDoc);
-                }.bind(this);
-                reader.readAsText(event.target.files[0]);
-
-                // Close Create Compendium message box
-                $(event.currentTarget).closest(".window-content").find("button").click();
+    async _onCreateEntry(event, target) {
+        try {
+            event.preventDefault();
+            event.stopPropagation();
+            const li = event.currentTarget.closest(".directory-item");
+            const targetFolderId = li ? li.dataset.folderId : null;
+            const types = CONST.COMPENDIUM_DOCUMENT_TYPES.map((documentName) => {
+                return {
+                    value: documentName,
+                    label: game.i18n.localize(getDocumentClass(documentName).metadata.label),
+                };
             });
-        }
+            game.i18n.sortObjects(types, "label");
+            const folders = this.collection._formatFolderSelectOptions();
+            let html = await renderTemplate("templates/sidebar/compendium-create.html", {
+                types,
+                folders,
+                folder: targetFolderId,
+                hasFolders: folders.length >= 1,
+            });
+            html = html.replace(
+                "Document.</p>",
+                `Document.</p><label>Hero Designer Prefab</label><input name="upload" class="upload" type="file" accept=".hdp"></input>`,
+            );
 
-        return new Dialog({
-            title: game.i18n.localize("COMPENDIUM.Create"),
-            content: html,
-            //label: game.i18n.localize("COMPENDIUM.Create"),
-            buttons: {
-                create: {
-                    label: game.i18n.localize("COMPENDIUM.Create"),
-                    callback: async (html) => {
-                        const form = html.find("#compendium-create")[0];
-                        const fd = new FormDataExtended(form);
-                        const metadata = fd.object;
+            function handleRender(html) {
+                html.on("change", "input.upload", (event) => {
+                    console.log(event, event.target.files[0]);
+                    const reader = new FileReader();
+                    reader.onload = async function (event) {
+                        const contents = event.target.result;
 
-                        if (metadata.upload) {
-                            return; // We handle this in the onchage event above because browser security use fakepath
-                        }
+                        const parser = new DOMParser();
+                        const xmlDoc = parser.parseFromString(contents, "text/xml");
+                        // TODO create the Item compendium
+                        HeroSystem6eCompendiumDirectory.uploadFromXml(xmlDoc);
+                    }.bind(this);
+                    reader.readAsText(event.target.files[0]);
 
-                        let targetFolderId = metadata.folder;
-                        if (metadata.folder) delete metadata.folder;
-                        if (!metadata.label) {
-                            let defaultName = game.i18n.format("DOCUMENT.New", {
-                                type: game.i18n.localize("PACKAGE.TagCompendium"),
-                            });
-                            const count = game.packs.size;
-                            if (count > 0) defaultName += ` (${count + 1})`;
-                            metadata.label = defaultName;
-                        }
-                        const pack =
-                            // eslint-disable-next-line no-undef
-                            await CompendiumCollection.createCompendium(metadata);
-                        if (targetFolderId) await pack.setFolder(targetFolderId);
+                    // Close Create Compendium message box
+                    $(event.currentTarget).closest(".window-content").find("button").click();
+                });
+            }
+
+            return new Dialog({
+                title: game.i18n.localize("COMPENDIUM.Create"),
+                content: html,
+                //label: game.i18n.localize("COMPENDIUM.Create"),
+                buttons: {
+                    create: {
+                        label: game.i18n.localize("COMPENDIUM.Create"),
+                        callback: async (html) => {
+                            const form = html.find("#compendium-create")[0];
+                            const fd = new FormDataExtended(form);
+                            const metadata = fd.object;
+
+                            if (metadata.upload) {
+                                return; // We handle this in the onchage event above because browser security use fakepath
+                            }
+
+                            let targetFolderId = metadata.folder;
+                            if (metadata.folder) delete metadata.folder;
+                            if (!metadata.label) {
+                                let defaultName = game.i18n.format("DOCUMENT.New", {
+                                    type: game.i18n.localize("PACKAGE.TagCompendium"),
+                                });
+                                const count = game.packs.size;
+                                if (count > 0) defaultName += ` (${count + 1})`;
+                                metadata.label = defaultName;
+                            }
+                            const pack =
+                                // eslint-disable-next-line no-undef
+                                await CompendiumCollection.createCompendium(metadata);
+                            if (targetFolderId) await pack.setFolder(targetFolderId);
+                        },
                     },
                 },
-            },
-            render: handleRender,
-            rejectClose: false,
-            options: { jQuery: false },
-        }).render(true);
+                render: handleRender,
+                rejectClose: false,
+                options: { jQuery: false },
+            }).render(true);
+        } catch (e) {
+            console.error(e);
+            super._onCreateEntry(event, target);
+        }
     }
 
     static async uploadFromXml(xml, targetFolderId) {
