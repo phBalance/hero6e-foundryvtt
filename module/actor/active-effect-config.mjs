08477d9a0f97ef4bb968bd5a6ffbba74ae7e873e
aeauseth <aeauseth@users.noreply.github.com>
2025-05-25 11:07:58 -0700

    V13 HeroSystemActiveEffectConfig #2297

--- module/actor/active-effect-config.mjs
+++ module/actor/active-effect-config.mjs
@@ -4,6 +4,7 @@ import { HEROSYS } from "../herosystem6e.mjs";
 const FoundryVttActiveEffectConfig = foundry.applications?.sheets?.ActiveEffectConfig || ActiveEffectConfig;
 
 export class HeroSystemActiveEffectConfig extends FoundryVttActiveEffectConfig {
+    // V12 static get defaultOptions is replaced by V13 static DEFAULT_OPTIONS = {}
     static get defaultOptions() {
         const defaultOptions = super.defaultOptions;
         return foundry.utils.mergeObject(defaultOptions, {
@@ -16,6 +17,112 @@ export class HeroSystemActiveEffectConfig extends FoundryVttActiveEffectConfig {
         });
     }
 
+    addHeroListeners(html, context) {
+        const detailsSection = html.find("section[data-tab='details']")?.[0];
+        if (detailsSection) {
+            const originItem = fromUuidSync(context.source.origin);
+            const token = fromUuidSync(context.source.origin?.match(/(.*).Actor/)?.[1]);
+            const originText =
+                (originItem ? `${token?.name || originItem.actor?.name}: ${originItem.name}` : context.source.origin) ||
+                "";
+
+            detailsSection.append(
+                $(`
+                <fieldset>
+                    <div class="form-group">
+                        <label>HERO.Origin</label>
+                        <div class="form-fields">
+                            <input type="text" name="originText" value="${originText}" disabled/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>HERO.XMLID</label>
+                        <div class="form-fields">
+                            <input type="text" name="flags.${game.system.id}.XMLID" value="${context.source.flags[game.system.id]?.XMLID || ""}" disabled/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>HERO.key</label>
+                        <div class="form-fields">
+                            <input type="text" name="flags.${game.system.id}.key" value="${context.source.flags[game.system.id]?.key || ""}" disabled/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>HERO.adjustmentActivePoints</label>
+                        <div class="form-fields">
+                            <input type="text" name="flags.${game.system.id}.adjustmentActivePoints" value="${context.source.flags[game.system.id]?.adjustmentActivePoints || ""}" disabled/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>HERO.source</label>
+                        <div class="form-fields">
+                            <input type="text" name="flags.${game.system.id}.source" value="${context.source.flags[game.system.id]?.source || ""}" disabled/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>HERO.target</label>
+                        <div class="form-fields">
+                            <input type="text" name="flags.${game.system.id}.target" value="${context.source.flags[game.system.id]?.target || ""}" disabled/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>HERO.targetDisplay</label>
+                        <div class="form-fields">
+                            <input type="text" name="flags.${game.system.id}.targetDisplay" value="${context.source.flags[game.system.id]?.targetDisplay || ""}" disabled/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>HERO.type</label>
+                        <div class="form-fields">
+                            <input type="text" name="flags.${game.system.id}.type" value="${context.source.flags[game.system.id]?.type || ""}" disabled/>
+                        </div>
+                    </div>
+                </fieldset>
+                `)[0],
+            );
+        }
+
+        const durationSection = html.find("section[data-tab='duration']")?.[0];
+        if (durationSection) {
+            const remaining = context.source.duration.startTime + context.source.duration.seconds - game.time.worldTime;
+            const startTimeDisplay = new Date(context.source.duration.startTime * 1000)
+                .toUTCString()
+                .replace(" GMT", "");
+
+            durationSection.append(
+                $(`
+                <fieldset>
+                    <div class="form-group">
+                        <label>HERO.Fade in (seconds)</label>
+                        <div class="form-fields">
+                            <input type="text" value="${remaining}" disabled/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>HERO.startTime</label>
+                        <div class="form-fields">
+                            <input type="text" value="${context.source.duration.startTime}" disabled/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>HERO.startTimeDisplay</label>
+                        <div class="form-fields">
+                            <input type="text" value="${startTimeDisplay}" disabled/>
+                        </div>
+                    </div>
+                </fieldset>
+                `)[0],
+            );
+        }
+    }
+
+    // v13 uses _onRender instead of activateListeners
+    _onRender(context, options) {
+        super._onRender(context, options);
+        this.addHeroListeners.call(this, $(this.element), context);
+    }
+
+    // V12 uses getData, V13 does not as it is a V2 application
     async getData() {
         const context = await super.getData();
 
