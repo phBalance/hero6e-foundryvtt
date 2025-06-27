90e47f30eabcabcadd347d20db4097fc849f1770
aeauseth <aeauseth@users.noreply.github.com>
2025-06-27 15:20:37 -0700

    V13: Built in ruler movement type + speed #2374 #2366

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,7 @@
 - Updated hitlocations labels. [#2403](https://github.com/dmdorman/hero6e-foundryvtt/issues/2403)
 - The STUNNED condition now only expires once per segment for actors with Lightning Reflexes. [#2404](https://github.com/dmdorman/hero6e-foundryvtt/issues/2404)
 - Generic damage roller now includes HIT LOCATION [#2398](https://github.com/dmdorman/hero6e-foundryvtt/issues/2398)
+- Improved V13 support for built in ruler. Movement selection + speed distance colors. [#2374](https://github.com/dmdorman/hero6e-foundryvtt/issues/2374) [#2366](https://github.com/dmdorman/hero6e-foundryvtt/issues/2366)
 
 ## Version 4.1.5 20250607
 
--- module/heroTokenRuler.mjs
+++ module/heroTokenRuler.mjs
@@ -17,22 +17,57 @@ export class HeroTokenRuler extends FoundryTokenRuler {
                 "-=blink": null,
                 burrow: {
                     canSelect: (token) => parseInt(token.actor?.system.characteristics.tunneling?.value) > 0,
+                    speed: (token) => parseInt(token.actor?.system.characteristics.tunneling?.value) || 0,
                 },
                 "-=crawl": null,
 
                 fly: {
                     canSelect: (token) => parseInt(token.actor?.system.characteristics.flight?.value) > 0,
+                    speed: (token) => parseInt(token.actor?.system.characteristics.flight?.value) || 0,
                 },
                 jump: {
                     canSelect: (token) => parseInt(token.actor?.system.characteristics.leaping?.value) > 0,
+                    speed: (token) => parseInt(token.actor?.system.characteristics.leaping?.value) || 0,
                 },
                 swim: {
                     canSelect: (token) => parseInt(token.actor?.system.characteristics.swimming?.value) > 0,
+                    speed: (token) => parseInt(token.actor?.system.characteristics.swimming?.value) || 0,
                 },
                 teleport,
                 // Swinging
+                walk: {
+                    speed: (token) => parseInt(token.actor?.system.characteristics.running?.value) || 0,
+                },
             },
             { performDeletions: true },
         );
+    }
+
+    _getGridHighlightStyle(waypoint, offset) {
+        const style = super._getGridHighlightStyle(waypoint, offset);
+        this.#speedValueStyle(style, waypoint);
+        return style;
+    }
+
+    /// Adjusts the grid or segment style based on the token's movement characteristics
+    #speedValueStyle(style, waypoint) {
+        const colors = [0x33bc4e, 0xf1d836, 0xe72124];
+
+        const movementCost = Math.floor(waypoint.measurement.cost);
+
+        let speed = waypoint.actionConfig.speed?.(this.token) ?? Infinity;
+        if (speed % 2 !== 2) {
+            speed += 1;
+        }
+        let index = 2;
+
+        if (movementCost <= speed) {
+            index = 1;
+        }
+        if (movementCost <= speed / 2) {
+            index = 0;
+        }
+
+        style.color = colors[index];
     }
 }
