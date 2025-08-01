fd9c33952225f01d6fed5b3d9c6f8cedc63203a6
aeauseth <aeauseth@users.noreply.github.com>
2025-08-01 14:34:32 -0700

    improved actorless handling

--- module/combatant.mjs
+++ module/combatant.mjs
@@ -17,6 +17,10 @@ export class HeroSystem6eCombatant extends Combatant {
 
     hasPhase(segmentNumber) {
         const actor = this.actor;
+        if (!actor) {
+            console.debug(`${this.name} has no actor`);
+            return segmentNumber === 12;
+        }
         const index = Math.min(Math.max(actor.system.characteristics.spd?.value || 0, 1), 12);
         const phases = foundry.utils.deepClone(HeroSystem6eCombatant.Speed2Segments[index]);
 
