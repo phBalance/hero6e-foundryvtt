3e705191b05be175eeb1f0c46ce9aa7c0e759333
aeauseth <aeauseth@users.noreply.github.com>
2025-08-01 13:14:29 -0700

    LR support for single combatant

--- module/actor/actor.mjs
+++ module/actor/actor.mjs
@@ -3363,13 +3363,17 @@ export class HeroSystem6eActor extends Actor {
     }
 
     getBaseInit(segmentNumber) {
-        if (segmentNumber != this.segmentNumber) {
-            const characteristic = this.system?.initiativeCharacteristic || "dex";
-            const initValue = this.system.characteristics[characteristic]?.value || 0;
-            const r = Math.floor(Math.random(6)) + 1;
-            this.currentInit = parseInt(initValue) + Number((r / 10).toFixed(2));
-            this.segmentNumber = segmentNumber;
-        }
-        return this.currentInit;
+        const characteristic = this.system?.initiativeCharacteristic || "dex";
+        const initValue = this.system.characteristics[characteristic]?.value || 0;
+        return parseInt(initValue);
+
+        // if (segmentNumber != this.segmentNumber) {
+        //     const characteristic = this.system?.initiativeCharacteristic || "dex";
+        //     const initValue = this.system.characteristics[characteristic]?.value || 0;
+        //     const r = Math.floor(Math.random(6)) + 1;
+        //     this.currentInit = parseInt(initValue) + Number((r / 10).toFixed(2));
+        //     this.segmentNumber = segmentNumber;
+        // }
+        // return this.currentInit;
     }
 }
