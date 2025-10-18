5b015d5fba5f098705677d5dec3c61c5c3222a11
Aaron Auseth <eauseth@gmail.com>
2025-10-18 11:58:58 -0700

    -v/10 #2938

--- module/item/HeroSystem6eTypeDataModels.mjs
+++ module/item/HeroSystem6eTypeDataModels.mjs
@@ -719,16 +719,17 @@ export class HeroSystem6eItemTypeDataModelGetters extends foundry.abstract.TypeD
             _details.tags.push({ name: this.item.name, value: parseInt(this[propUpper]) });
         }
 
-        // if (this[propUpper] === "-v/10") {
-        //     // Educated guess for token
-        //     const token =
-        //         this.actor.getActiveTokens().find((t) => canvas.tokens.controlled.find((c) => c.id === t.id)) ||
-        //         this.actor.getActiveTokens()[0];
-        //     const velocity = calculateVelocityInSystemUnits(this.actor, token);
-        //     if (velocity !== 0) {
-        //         _details.tags.push({ name: "Velocity", value: -parseInt(velocity / 10) });
-        //     }
-        // }
+        // Unclear when this would get used (maneuver?)
+        if (this[propLower] === "-v/10") {
+            // Educated guess for token
+            const token =
+                this.actor?.getActiveTokens().find((t) => canvas.tokens.controlled.find((c) => c.id === t.id)) ||
+                this.actor?.getActiveTokens()[0];
+            const velocity = calculateVelocityInSystemUnits(this.actor, token);
+            if (velocity !== 0) {
+                _details.tags.push({ name: "Velocity", value: -parseInt(velocity / 10) });
+            }
+        }
 
         for (const cslDetail of combatSkillLevelsForAttack(this.item).details) {
             if (cslDetail[propLower]) {
