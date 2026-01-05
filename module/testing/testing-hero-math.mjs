644cd1c61883e3e9e4863a140713cdf742d307b2
phBalance <29824554+phBalance@users.noreply.github.com>
2026-01-05 10:00:19 -0700

    fix(rounding): support negative numbers and be precise in language

--- module/testing/testing-hero-math.mjs
+++ module/testing/testing-hero-math.mjs
@@ -1,5 +1,5 @@
 import {
-    numberToOneDecimalPlace,
+    numberToOneDecimalDigit,
     roundFavorPlayerTowardsZero,
     roundFavorPlayerAwayFromZero,
 } from "../utility/round.mjs";
@@ -13,114 +13,130 @@ export function registerHeroMathTests(quench) {
             describe("numberToOneDecimalPlace", function () {
                 describe("integer numbers", function () {
                     it("should not change -2000000", function () {
-                        expect(numberToOneDecimalPlace(-2000000)).to.equal(-2000000);
+                        expect(numberToOneDecimalDigit(-2000000)).to.equal(-2000000);
                     });
 
                     it("should not change -2", function () {
-                        expect(numberToOneDecimalPlace(-2)).to.equal(-2);
+                        expect(numberToOneDecimalDigit(-2)).to.equal(-2);
                     });
 
                     it("should not change 0", function () {
-                        expect(numberToOneDecimalPlace(0)).to.equal(0);
+                        expect(numberToOneDecimalDigit(0)).to.equal(0);
                     });
 
                     it("should not change 2", function () {
-                        expect(numberToOneDecimalPlace(2)).to.equal(2);
+                        expect(numberToOneDecimalDigit(2)).to.equal(2);
                     });
 
                     it("should not change 2000000", function () {
-                        expect(numberToOneDecimalPlace(2000000)).to.equal(2000000);
+                        expect(numberToOneDecimalDigit(2000000)).to.equal(2000000);
                     });
                 });
 
                 describe("numbers with 1 decimal", function () {
                     it("should not change -2.1", function () {
-                        expect(numberToOneDecimalPlace(-2.1)).to.equal(-2.1);
+                        expect(numberToOneDecimalDigit(-2.1)).to.equal(-2.1);
                     });
 
                     it("should not change -1.9", function () {
-                        expect(numberToOneDecimalPlace(-1.9)).to.equal(-1.9);
+                        expect(numberToOneDecimalDigit(-1.9)).to.equal(-1.9);
                     });
 
                     it("should not change 0.1", function () {
-                        expect(numberToOneDecimalPlace(0.1)).to.equal(0.1);
+                        expect(numberToOneDecimalDigit(0.1)).to.equal(0.1);
                     });
 
                     it("should not change 1.9", function () {
-                        expect(numberToOneDecimalPlace(1.9)).to.equal(1.9);
+                        expect(numberToOneDecimalDigit(1.9)).to.equal(1.9);
                     });
 
                     it("should not change 2.1", function () {
-                        expect(numberToOneDecimalPlace(2.1)).to.equal(2.1);
+                        expect(numberToOneDecimalDigit(2.1)).to.equal(2.1);
                     });
                 });
 
                 describe("numbers with 2 decimals", function () {
                     it("should change -1.91", function () {
-                        expect(numberToOneDecimalPlace(-1.91)).to.equal(-1.9);
+                        expect(numberToOneDecimalDigit(-1.91)).to.equal(-1.9);
                     });
 
                     it("should change -1.99", function () {
-                        expect(numberToOneDecimalPlace(-1.99)).to.equal(-1.9);
+                        expect(numberToOneDecimalDigit(-1.99)).to.equal(-1.9);
                     });
 
                     it("should change 1.91", function () {
-                        expect(numberToOneDecimalPlace(1.91)).to.equal(1.9);
+                        expect(numberToOneDecimalDigit(1.91)).to.equal(1.9);
                     });
 
                     it("should change 1.99", function () {
-                        expect(numberToOneDecimalPlace(1.99)).to.equal(1.9);
+                        expect(numberToOneDecimalDigit(1.99)).to.equal(1.9);
                     });
                 });
 
                 describe("numbers with 3 decimals", function () {
                     it("should change -1.501", function () {
-                        expect(numberToOneDecimalPlace(-1.501)).to.equal(-1.5);
+                        expect(numberToOneDecimalDigit(-1.501)).to.equal(-1.5);
                     });
 
                     it("should change -1.499", function () {
-                        expect(numberToOneDecimalPlace(-1.499)).to.equal(-1.4);
+                        expect(numberToOneDecimalDigit(-1.499)).to.equal(-1.4);
+                    });
+
+                    it("should change -0.999", function () {
+                        expect(numberToOneDecimalDigit(-0.999)).to.equal(-0.9);
+                    });
+
+                    it("should change 0.999", function () {
+                        expect(numberToOneDecimalDigit(0.999)).to.equal(0.9);
                     });
 
                     it("should change 1.499", function () {
-                        expect(numberToOneDecimalPlace(1.499)).to.equal(1.4);
+                        expect(numberToOneDecimalDigit(1.499)).to.equal(1.4);
                     });
 
                     it("should not change 1.501", function () {
-                        expect(numberToOneDecimalPlace(1.501)).to.equal(1.5);
+                        expect(numberToOneDecimalDigit(1.501)).to.equal(1.5);
                     });
                 });
 
                 describe("calculations", function () {
                     it("should change 8/1.75", function () {
                         // 8 / 1.75 = 4.571428571428571 => 4.5
-                        expect(numberToOneDecimalPlace(8 / 1.75)).to.equal(4.5);
+                        expect(numberToOneDecimalDigit(8 / 1.75)).to.equal(4.5);
                     });
 
                     it("should change 8/2.25", function () {
                         // 8 / 2.25 = 3.5556 => 3.5
-                        expect(numberToOneDecimalPlace(8 / 2.25)).to.equal(3.5);
+                        expect(numberToOneDecimalDigit(8 / 2.25)).to.equal(3.5);
                     });
 
                     it("should change 8/3.25", function () {
                         // 8 / 3.25 = 2.4615 => 2.4
-                        expect(numberToOneDecimalPlace(8 / 3.25)).to.equal(2.4);
+                        expect(numberToOneDecimalDigit(8 / 3.25)).to.equal(2.4);
                     });
 
                     it("should change 13/1.75", function () {
                         // 13 / 1.75 => 7.4286 => 7.4
-                        expect(numberToOneDecimalPlace(13 / 1.75)).to.equal(7.4);
+                        expect(numberToOneDecimalDigit(13 / 1.75)).to.equal(7.4);
                     });
 
                     it("should change 29/2.75", function () {
                         // 29 / 2.75 => 10.5455 => 10.5
-                        expect(numberToOneDecimalPlace(29 / 2.75)).to.equal(10.5);
+                        expect(numberToOneDecimalDigit(29 / 2.75)).to.equal(10.5);
                     });
                 });
             });
 
             describe("Hero Rounding", function () {
                 describe("roundFavorPlayerAwayFromZero", function () {
+                    it("should round -1.5 to -2", function () {
+                        expect(roundFavorPlayerAwayFromZero(-1.5)).to.equal(-2);
+                    });
+
+                    it("should round -0.5 to -1", function () {
+                        expect(roundFavorPlayerAwayFromZero(-0.5)).to.equal(-1);
+                    });
+
                     it("should round 0.0 to 0", function () {
                         expect(roundFavorPlayerAwayFromZero(0.0)).to.equal(0);
                     });
@@ -149,12 +165,28 @@ export function registerHeroMathTests(quench) {
                         expect(roundFavorPlayerAwayFromZero(0.999)).to.equal(1);
                     });
 
+                    it("should round 1.5 to 2", function () {
+                        expect(roundFavorPlayerAwayFromZero(1.5)).to.equal(2);
+                    });
+
                     it("should round 10000000.5 to 10000001", function () {
                         expect(roundFavorPlayerAwayFromZero(10000000.5)).to.equal(10000001);
+                    });
+
+                    it("should round 10000001.5 to 10000002", function () {
+                        expect(roundFavorPlayerAwayFromZero(10000001.5)).to.equal(10000002);
                     });
                 });
 
                 describe("roundFavorPlayerTowardsZero", function () {
+                    it("should round -1.5 to -1", function () {
+                        expect(roundFavorPlayerTowardsZero(-1.5)).to.equal(-1);
+                    });
+
+                    it("should round -0.5 to 0", function () {
+                        expect(roundFavorPlayerTowardsZero(-0.5)).to.equal(0);
+                    });
+
                     it("should round 0.0 to 0", function () {
                         expect(roundFavorPlayerTowardsZero(0.0)).to.equal(0);
                     });
@@ -183,8 +215,16 @@ export function registerHeroMathTests(quench) {
                         expect(roundFavorPlayerTowardsZero(0.999)).to.equal(1);
                     });
 
+                    it("should round 1.5 to 1", function () {
+                        expect(roundFavorPlayerTowardsZero(1.5)).to.equal(1);
+                    });
+
                     it("should round 10000000.5 to 10000000", function () {
                         expect(roundFavorPlayerTowardsZero(10000000.5)).to.equal(10000000);
+                    });
+
+                    it("should round 10000001.5 to 10000001", function () {
+                        expect(roundFavorPlayerTowardsZero(10000001.5)).to.equal(10000001);
                     });
                 });
             });
