17d6eff8d952081fcb1cfb8826ca910b71b32d42
phBalance <29824554+phBalance@users.noreply.github.com>
2026-02-07 19:52:53 -0700

    feat(off hand defense): add off hand defense for 5e and 6e

---
+++ module/actor/actor-utils.mjs
@@ -0,0 +1,40 @@
+/**
+ * Get the bonus to DCV against Hand-To-Hand attacks.
+ *
+ * In 5e this defense is given by the Weapon Familiarity: Off Hand skill
+ * In 6e this defense is given by the Off Hand Defense talent
+ *
+ * @param {HeroSystem6eActor} defendingActor
+ *
+ * @returns number - Bonus to DCV against Hand-To-Hand attacks
+ */
+export function getOffHandDefenseDcv(defendingActor) {
+    let offHandDcvBonus = 0;
+
+    if (defendingActor) {
+        if (defendingActor.is5e) {
+            // Weapon Familiarity off hand can only be bought once
+            const activeWeaponFamiliarityItems = defendingActor.items.filter(
+                (item) => item.system.XMLID === "WEAPON_FAMILIARITY" && item.isActive,
+            );
+            for (const wfItem of activeWeaponFamiliarityItems) {
+                // Weapon Familiarity off hand can only be bought one.
+                // NOTE: This assumes that someone doesn't buy it multiple times in different WFs
+                const offHandWfItem = wfItem.adders.find((adder) => adder.XMLID === "OFFHAND");
+                if (offHandWfItem) {
+                    offHandDcvBonus += 1;
+                }
+            }
+        } else {
+            // It's possible for the skill to be purchased multiple times with GM permission so just support it.
+            const activeOffHandDefenseItems = defendingActor.items.filter(
+                (item) => item.system.XMLID === "OFFHANDDEFENSE" && item.isActive,
+            );
+            if (activeOffHandDefenseItems.length) {
+                offHandDcvBonus += activeOffHandDefenseItems.length;
+            }
+        }
+    }
+
+    return offHandDcvBonus;
+}
