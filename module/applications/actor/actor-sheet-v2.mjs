353d6a437bdc435ff196917d32c8e51bb82794f6
Nicholas Guarino <me@ndguarino.com>
2026-03-27 20:37:01 -0400

    feat(actor-sheet-v2/token-img): Improved display for unlinked tokens, fix for override of foundry token field

--- module/applications/actor/actor-sheet-v2.mjs
+++ module/applications/actor/actor-sheet-v2.mjs
@@ -2,12 +2,7 @@ import { getActorDefensesVsAttack } from "../../utility/defense.mjs";
 import { HeroSystem6eActor } from "../../actor/actor.mjs";
 import { HeroSystem6eItem } from "../../item/item.mjs";
 import { foundryVttParseUuid } from "../../utility/compatibility.mjs";
-import {
-    getPowerInfo,
-    getCharacteristicInfoArrayForActor,
-    tokenEducatedGuess,
-    whisperUserTargetsForActor,
-} from "../../utility/util.mjs";
+import { getPowerInfo, getCharacteristicInfoArrayForActor, whisperUserTargetsForActor } from "../../utility/util.mjs";
 
 const { HandlebarsApplicationMixin } = foundry.applications.api;
 const { ActorSheetV2 } = foundry.applications.sheets;
@@ -248,6 +243,7 @@ export class HeroSystemActorSheetV2 extends HandlebarsApplicationMixin(ActorShee
     }
 
     get title() {
+        if (this.token) return `${this.actor.type.toUpperCase()}: ${this.token.name} [${this.actor.name}]`;
         return `${this.actor.type.toUpperCase()}: ${this.actor.name}`;
     }
 
@@ -345,12 +341,6 @@ export class HeroSystemActorSheetV2 extends HandlebarsApplicationMixin(ActorShee
                 scrollable: [""],
             },
         };
-    }
-
-    #token;
-
-    get token() {
-        return this.document.token ?? this.#token ?? tokenEducatedGuess({ actor: this.actor });
     }
 
     static TABS = {
@@ -740,9 +730,6 @@ export class HeroSystemActorSheetV2 extends HandlebarsApplicationMixin(ActorShee
 
     async _onFirstRender(context, options) {
         await super._onFirstRender(context, options);
-
-        // Keep track of token; needed for linked actors
-        this.#token = options.token ?? tokenEducatedGuess({ actor: this.actor });
 
         // General right click on row
         this._createContextMenu(this._getDocumentListContextOptions, "[data-document-uuid]", {
