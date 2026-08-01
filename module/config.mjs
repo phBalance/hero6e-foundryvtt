b83997ad098f708f2dbdfc086cd702d8af7b16a2
phBalance <29824554+phBalance@users.noreply.github.com>
2026-08-01 08:38:19 -0600

    feat(config): add RECEIVEONLY for TELEPATHY

--- module/config.mjs
+++ module/config.mjs
@@ -19181,6 +19181,18 @@ function addPower(powerDescription6e, powerOverrideFor5e) {
     );
     addPower(
         {
+            // TELEPATHY related
+            key: "RECEIVEONLY",
+            behaviors: ["modifier"],
+            type: ["modifier"],
+            costPerLevel: fixedValueFunction(0),
+            dcAffecting: fixedValueFunction(false),
+            xml: `<MODIFIER XMLID="RECEIVEONLY" ID="1785043243974" BASECOST="-0.5" LEVELS="0" ALIAS="Receive Only" POSITION="-1" MULTIPLIER="1.0" GRAPHIC="Burst" COLOR="255 255 255" SFX="Default" SHOW_ACTIVE_COST="Yes" INCLUDE_NOTES_IN_PRINTOUT="Yes" NAME="" COMMENTS="" PRIVATE="No" FORCEALLOW="No"></MODIFIER>`,
+        },
+        {},
+    );
+    addPower(
+        {
             key: "REDUCEDBYRANGE",
             behaviors: ["modifier"],
             type: ["modifier"],
